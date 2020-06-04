9e15db341acf4a5e97470ccd5e6139bad9031754
Ayman Azm <40792340+Ayman-Azm@users.noreply.github.com>
2020-06-04 23:19:38 +0100

    Update First_prototype.js
    Added a function for mining new blocks with a certain difficulty

--- First_prototype.js
+++ First_prototype.js
@@ -6,19 +6,28 @@ class Block {
         this.data = data;
         this.timestamp = timestamp;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.data)).digest('hex');
+        return crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
     }
 }
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock() {
-        return new Block(0, '2020-06-02', [], '0');
+        return new Block(0, '2020-02-29', [], '0');
     }
 
     getLatestBlock() {
@@ -27,7 +36,7 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
     isChainValid() {
@@ -45,14 +54,16 @@ class Blockchain {
         return true;
     }
 }
+
+//the rest of the code is for demonstration and testing purposes
 let hipcoin = new Blockchain();
-hipcoin.createGenesisBlock();
-hipcoin.addBlock(new Block(1, "2020-06-02", { amount: 10 }));
+console.log("Mining block....")
+hipcoin.addBlock(new Block(1, "2020-02-30", { amount: 10 }));
 
 console.log("voici ma blockchain " + JSON.stringify(hipcoin, null, 4));
-
-hipcoin.chain[1].data = { amount: 100 };
-
+console.log("mining block 2")
+hipcoin.addBlock(new Block(1, "2020-02-30", { amount: 20 }));
+hipcoin.chain[1].data={amount : 100};
 hipcoin.chain[1].hash = hipcoin.chain[1].calculateHash();
-console.log("voici ma blockchain " + JSON.stringify(hipcoin, null, 4));
+
 console.log("is Blockchain valid?" + hipcoin.isChainValid())
